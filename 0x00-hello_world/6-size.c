7d2cf416d2fffecdffd1d45572dbb173196acada
Zaeemudeen <142699408+Zaeemudeen@users.noreply.github.com>
2023-09-07 19:16:50 +0100

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
 * main - entry point
 * program prints the size of functions
@@ -16,15 +15,11 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a))
-
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(a));
+ printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(b));
+ printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(c));
+ printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(d));
+ printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(e));
 
 	return 0;
 }

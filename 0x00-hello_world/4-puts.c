1d42d467091baaadde456416acc841929d907860
Zaeemudeen <zaeemulqurraa@gmail.com>
2023-09-07 08:09:48 -0700

    #include<stdio.h>
    /**
      * main - entry point
      * prints "Programming is like building a multilingual puzzle
      * Return 0
      */
    
    int main(void)
    
    {
    puts("\"Programming is like building a multilingual puzzle");
    return (0);

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
-int main(void)
-
 /**
   * main - entry point
   * prints "Programming is like building a multilingual puzzle
   * Return 0
-*/
+  */
   
+int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

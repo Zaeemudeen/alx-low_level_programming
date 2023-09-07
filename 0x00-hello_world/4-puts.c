e486d6d84a064906fd8536544d94e3ea4dabb4ba
Zaeemudeen <zaeemulqurraa@gmail.com>
2023-09-07 07:57:55 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,12 @@
 
 int main(void)
 
+/**
+  * main - entry point
+  * prints "Programming is like building a multilingual puzzle
+  * Return 0
+  */
+  
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

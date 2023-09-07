81ca099e471ae26c67d3d6508ddfe02a02e228c4
Zaeemudeen <zaeemulqurraa@gmail.com>
2023-09-07 09:41:11 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,6 +9,6 @@
 int main(void)
 
 {
-	printf(stderr,"with proper grammar, but the outcome is a piece of art, ");
+	fprintf(stderr,"with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }

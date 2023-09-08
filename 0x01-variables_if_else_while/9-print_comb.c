0eb3940088481b3cf624024a7c2d31c624d18bd7
Zaeemudeen <142699408+Zaeemudeen@users.noreply.github.com>
2023-09-08 20:56:32 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -19,13 +19,13 @@ int main(void)
 
 			for (a = 48; a < 58 ; a++)
 			{
-				putchar (a);
+				putchar(a);
 
 				if (a != 57)
-				putchar (44);
-				putchar (32);
+				putchar(44);
+				putchar(32);
 			}
-			putchar ('\n');
+			putchar('\n');
 			return (0);
 }
 

413dfaad172355f3dd71decc84c16c3db2ee213c
Zaeemudeen <142699408+Zaeemudeen@users.noreply.github.com>
2023-09-09 00:58:25 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,41 +10,41 @@
 
 int main(void)
 {
-		int n = 48;
-		int m = 49;
-		int w = 50;
-		int c = 49;
-		int d = 50;
-		int b = 50;
+	int n = 48;
+	int m = 49;
+	int w = 50;
+	int c = 49;
+	int d = 50;
+	int b = 50;
 
-		while (n < 58)
+	while (n < 58)
+	{
+	while (m < 58)
+	{
+	while (w < 58)
+	{
+	putchar(n);
+	putchar(m);
+	putchar(w);
+		if ((n != 55))
 		{
-				while (m < 58)
-				{
-						while (w < 58)
-						{
-								putchar(n);
-								putchar(m);
-								putchar(w);
-										if ((n != 55))
-										{
-											putchar(44);
-											putchar(32);
-										}
-								w++;
-						}
-								m++;
-								d++;
-								w = d;
-					}
-						c++;
-						b++;
-						d = b;
-						m = c;
-						w = b;
-						n++;
+			putchar(44);
+			putchar(32);
 		}
+	w++;
+	}
+	m++;
+	d++;
+	w = d;
+	}
+	c++;
+	b++;
+	d = b;
+	m = c;
+	w = b;
+	n++;
+	}
 
-		putchar('\n');
-		return (0);
+	putchar('\n');
+	return (0);
 }

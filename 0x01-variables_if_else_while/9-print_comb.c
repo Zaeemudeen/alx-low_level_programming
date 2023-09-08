180e913c2b6017718781ff735c1b9cde2a1eb270
Zaeemudeen <142699408+Zaeemudeen@users.noreply.github.com>
2023-09-08 20:35:00 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,30 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
 
+/**
+* main - entry point
+* program prints the size of functions
+* Return: always 0 (successful)
+*/
 
+int main(void)
+
+{
+			int a;
+
+			for (a = 48; a <= 57 ; a++)
+			{
+				putchar (a);
+
+				if (a != 57)
+				putchar (44);
+				putchar (32);
+			}
+			putchar ('\n');
+			return (0);
+}

7d20ec5f256ec4ad374693f18335bbab8ca05d05
Zaeemudeen <142699408+Zaeemudeen@users.noreply.github.com>
2023-09-08 21:32:30 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,23 +8,23 @@
 int main(void)
 
 {
-					int m;
+						int m;
 
-					for (m = 48; m < 57; m++)
-					{
-							int n;
+						for (m = 48; m < 57; m++)
+						{
+						int n;
 
-							for (n = m + 1; n < 58; n++)
-							{
-							putchar(m);
-							putchar(n);
+						for (n = m + 1; n < 58; n++)
+						{
+						putchar(m);
+						putchar(n);
 
-							if (m == 56 && n == 57)
-							continue;
-							putchar(44);
-							putchar(32);
-							}
-					}
-					putchar('\n');
-					return (0);
+						if (m == 56 && n == 57)
+						continue;
+						putchar(44);
+						putchar(32);
+						}
+						}
+						putchar('\n');
+						return (0);
 }

626030abd3fac35abdeae31ea24602fa321c74f2
Zaeemudeen <zaeemulqurraa@gmail.com>
2023-09-08 10:38:53 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+* main - entry point
+* program prints the size of functions
+* Return: always 0 (successful)
+*/
+
+ 
+int main () {
+
+		/* local variable definition */
+		int a = 0;
+
+		/* while loop execution */
+		while ( a < 10 ){
+				printf("%d", a);
+				a++;
+				}
+				printf("\n");
+
+		return 0;
+}
+

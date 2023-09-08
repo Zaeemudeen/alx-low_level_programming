1140d8781e17c003924a59e0f80c481d78a51b9a
Zaeemudeen <zaeemulqurraa@gmail.com>
2023-09-08 11:03:23 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+* main - entry point
+* program prints the size of functions
+* Return: always 0 (successful)
+*/
+
+int main(void)
+
+{
+			int a;
+
+			for (a = '0'; a <= '9'; a++)
+			putchar (a);
+			putchar ('\n');
+			return (0);
+}
+

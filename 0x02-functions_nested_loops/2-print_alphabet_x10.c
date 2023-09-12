f5ab18a176603fec1102b5e0cafaf2162634ced8
Zaeemudeen <142699408+Zaeemudeen@users.noreply.github.com>
2023-09-12 12:49:39 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,7 @@
 /**
 * main - Prints the alphabets in lower case 10x
 *
-* print_alphabet_x10 - prints alphabets 10 times.
+* print_alphabet_x10:prints alphabets 10 times.
 */
 
 void print_alphabet_x10(void)

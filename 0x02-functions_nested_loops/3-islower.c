2539e669c20acbfe3b4b3b2981fd1ad69065e23e
Zaeemudeen <142699408+Zaeemudeen@users.noreply.github.com>
2023-09-12 14:27:13 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,7 @@
 
 /**
   * _islower - this function checks if a character is a lowercase letter.
-  * @c - this is the character to be checked
+  * c: this is the character to be checked
   * Return: Returns 1 if the character is a lowercase letter, and 0 otherwise.
   */
 

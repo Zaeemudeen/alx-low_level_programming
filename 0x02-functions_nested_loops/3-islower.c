6d0b3b21f5d4608c7a43c68cb8aad730887fee51
Zaeemudeen <142699408+Zaeemudeen@users.noreply.github.com>
2023-09-12 13:22:54 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,27 +3,8 @@
 #include "main.h"
 
 /**
-   * _islower - a function that checks if a letter is lower or uppercase
-   */
-
-int _islower(int c);
-
-int main(void)
-{
-    int r;
-		r = _islower('H');
-		putchar(r + '0');
-
-		r = _islower('o');
-		putchar(r + '0');
-
-		r = _islower(108);
-		putchar(r + '0');
-		
-		putchar('\n');
-		
-		return (0);
-}
+  * _islower - a function that checks if a letter is lower or uppercase
+  */
 
 int _islower(int c)
 

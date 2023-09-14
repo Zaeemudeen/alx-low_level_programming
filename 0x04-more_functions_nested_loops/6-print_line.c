20b5908fcb679db4578f98dccfcb6e2fa25ea733
Zaeemudeen <zaeemulqurraa@gmail.com>
2023-09-14 16:38:41 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_line - prints line in numbers of n
+* @n: number of lines to print
+* Return: Always 0
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n < 1)
+	putchar('\n');
+	
+	else
+	for (i = 1;i <= n; i++)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}
+

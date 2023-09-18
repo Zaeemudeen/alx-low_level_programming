37b4ac0586d298cacc123361bae85db81614d048
Zaeemudeen <zaeemulqurraa@gmail.com>
2023-09-18 12:52:07 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts - print string to stdout
+ *
+ * @str: pointing to the string
+ * Return: Always zero.
+ */
+
+void _puts(char *str)
+{
+	int i;
+	i = 0;
+
+	if (str[i] != '\0')
+	{
+		puts(str);
+		i++;
+	}
+}
+

ea8ff18e53c44f3272477c60cc1c03100cb9256d
Zaeemudeen <zaeemulqurraa@gmail.com>
2023-09-20 16:41:12 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * leet - manipulates selected elements
+ * @str: the string
+ * Return: returns str
+ */
+
+char *leet(char *str)
+{
+	int i = 0, j;
+
+	while (str[i] != '\0')
+	{
+	i++;
+
+	for (j = 0; j < i; j++)
+	{
+		if (str[j] == 65 || str[j] == 97)
+		str[j] = 52;
+
+		if (str[j] == 69 || str[j] == 101)
+		str[j] = 51;
+
+		if (str[j] == 79 || str[j] == 111)
+		str[j] = 48;
+
+		if (str[j] == 84 || str[j] == 116)
+		str[j] = 55;
+
+		if (str[j] == 76 || str[j] == 108)
+		str[j] = 49;
+	}
+	}
+	return (str);
+}
+

7bfb4be238c1703d07d7c1aad66bf7f537d8ac3d
Zaeemudeen <zaeemulqurraa@gmail.com>
2023-09-20 15:21:37 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * cap_string - changes the first letters of string to uppper case
+ * @str: the string
+ * Return: returns str
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0, j, s;
+
+	while (str[i] != '\0')
+	{
+		i++;
+
+		for (j = 0; j < i; j++)
+		{
+		s = str[j - 1];
+
+		if (s == '\n' || s == '.' || s == ' ' || s == '\t')
+		str[j] = toupper(str[j]);
+		}
+	}
+	return (str);
+}
+

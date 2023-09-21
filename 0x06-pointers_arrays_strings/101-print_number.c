6b51b7c97b42696b54d2d4ddfb312cb65ca81c37
Zaeemudeen <zaeemulqurraa@gmail.com>
2023-09-21 04:49:41 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_number - print integer
+ * @n: the integer
+ * Return: Always 0.
+ */
+
+void print_number(int n)
+{
+	int x, y, count;
+
+	if (n < 0)
+	{
+		putchar(45);
+		x = n * -1;
+	}
+	else
+	{
+		x = n;
+	}
+
+	y = x;
+	count = 1;
+
+	while (y > 9)
+	{
+		y /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		putchar(((x / count) % 10) + 48);
+	}
+}
+

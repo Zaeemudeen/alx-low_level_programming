97355f82031a5129ba1d24a90de2fa9036beea17
Zaeemudeen <142699408+Zaeemudeen@users.noreply.github.com>
2023-09-25 21:38:19 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,8 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-		{
-		return (s);
-		}
-		s++;
-	}
-	return (NULL);
+	char str = strchr(s, c);
+
+	return (str);
 }
 

aa9140d1eec1f09ca0e96954ef449d7504a83074
Zaeemudeen <zaeemulqurraa@gmail.com>
2023-09-25 04:58:48 -0700

    memeset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - set the memory with the given value
+ * @s: the string
+ * @b: the value to set to the memory
+ * @n: the byte to reset
+ * Return: returns s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
+	return (s);
+}
+
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * memcpy - 
+ *
+ *
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)

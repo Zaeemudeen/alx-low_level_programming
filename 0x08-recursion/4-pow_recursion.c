4b86a5124ca82904e5f186551b1528b0eb514433
Zaeemudeen <142699408+Zaeemudeen@users.noreply.github.com>
2023-09-28 14:43:01 +0100

    Update 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - prints power of two integers
+ * @x: base number
+ * @y: exponential
+ * Return: power and -1 if y < 0
+ */
+ 
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (pow (x, y));
+}

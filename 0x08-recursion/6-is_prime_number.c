1c6b828d065fb8ae8b60b781dfbf5c7149f0db60
Zaeemudeen <142699408+Zaeemudeen@users.noreply.github.com>
2023-09-29 08:04:18 +0100

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,9 +1,9 @@
 include "main.h"
 
 /**
-	* prime2 - Makes possible to evaluate from 1 to n
-	* @a: same number as n
-	* @b: number that iterates from 1 to n
+ * prime2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
  * Return: On success 1
  * On error, -1 is returned, and errno is set appropriately.
  */

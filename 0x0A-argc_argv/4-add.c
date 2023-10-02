8247e22d899be8ccdbc31e41752baa394213139f
Zaeemudeen <142699408+Zaeemudeen@users.noreply.github.com>
2023-10-03 00:34:38 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -31,11 +31,10 @@ int _isnumber(char *s)
 
 /**
  * main - Entry point
-	*
-	* @argc: Counts the number of parameters that go into main
-	* @argv: Pointer of array of pointers containing strings entering main
-	* Return: Always 0 (Success)
-	*/
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return: Always 0 (Success)
+ */
 
 int main(int argc, char **argv)
 {

e9b74d6a187e889400b70d7cdc5d5e14e9a9a2ef
Zaeemudeen <zaeemulqurraa@gmail.com>
2023-10-03 07:53:33 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - duplicates string
+ * @str: the string to duplicate
+ * Return: pointer to the duplicate str
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+
+	s = strdup(str);
+	return (s);
+}
+

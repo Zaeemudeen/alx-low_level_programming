20994b6709ed1614363a5a7be14f903865723cff
Zaeemudeen <zaeemulqurraa@gmail.com>
2023-10-02 20:40:34 -0700

    libmy.a

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,7 @@
+#include <string.h>
+#include "main.h"
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	return(strncpy(dest, src, n));
+}
---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,7 @@
+#include <string.h>
+#include "main.h"
+
+char *_strpbrk(char *s, char *accept)
+{
+	return(strpbrk(s, accept));
+}

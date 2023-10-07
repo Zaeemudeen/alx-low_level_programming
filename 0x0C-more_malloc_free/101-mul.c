62c8e1909ec0e068aabd94c451e706d55e812071
Zaeemudeen <142699408+Zaeemudeen@users.noreply.github.com>
2023-10-07 12:28:11 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,22 +11,22 @@
  * Return: 1 if number, 0 if not
  */
 
-int _isnumber(char *s) 
- { 
-         int i, check, d; 
-  
-         d = 0, check = 1; 
-         for (i = 0; *(s + i) != 0; i++) 
-         { 
-                 d = isdigit(*(s + i)); 
-                 if (d == 0) 
-                 { 
-                         check = 0; 
-                         break; 
-                 } 
-         } 
-         return (check); 
- }
+int _isnumber(char *s)
+{
+	int i, check, d;
+
+	d = 0, check = 1;
+	for (i = 0; *(s + i) != 0; i++)
+	{
+		d = isdigit(*(s + i));
+		if (d == 0)
+		{
+			check = 0;
+			break;
+		}
+	}
+	return (check);
+}
 
 /**
  * _callocX - reserves memory initialized to 0
@@ -36,36 +36,37 @@ int _isnumber(char *s)
  * Return: pointer
  */
 
-char *_callocX(unsigned int nmemb) 
- { 
-         unsigned int i; 
-         char *p; 
-  
-         p = malloc(nmemb + 1); 
-         if (p == 0) 
-                 return (0); 
-         for (i = 0; i < nmemb; i++) 
-                 p[i] = '0'; 
-         p[i] = '\0'; 
-         return (p); 
- } 
-  
- /** 
-  * main - multiplies inf numbers 
-  * 
-  * @argc: # of cmd line args 
-  * @argv: cmd line args 
-  * Return: No return 
-  */ 
- int main(int argc, char **argv) 
- { 
-         int i, j, l1, l2, lful, mul, add, ten, ten2, tl, zer = 0; 
-         char *res; 
-  
-         if (argc != 3 || _isnumber(argv[1]) == 0 || _isnumber(argv[2]) == 0) 
-                 printf("Error\n"), exit(98); 
-         if (atoi(argv[1]) == 0 || atoi(argv[2]) == 0) 
-                 printf("0\n"), exit(0); 
+char *_callocX(unsigned int nmemb)
+{
+	unsigned int i;
+	char *p;
+
+	p = malloc(nmemb + 1);
+	if (p == 0)
+		return (0);
+	for (i = 0; i < nmemb; i++)
+		p[i] = '0';
+	p[i] = '\0';
+	return (p);
+}
+
+/**
+ * main - multiplies inf numbers
+ *
+ * @argc: # of cmd line args
+ * @argv: cmd line args
+ * Return: No return
+ */
+int main(int argc, char **argv)
+{
+	int i, j, l1, l2, lful, mul, add, ten, ten2, tl, zer = 0;
+	char *res;
+
+	if (argc != 3 || _isnumber(argv[1]) == 0 || _isnumber(argv[2]) == 0)
+		printf("Error\n"), exit(98);
+
+	if (atoi(argv[1]) == 0 || atoi(argv[2]) == 0)
+		printf("0\n"), exit(0); 
          l1 = strlen(argv[1]), l2 = strlen(argv[2]); 
          lful = l1 + l2; 
          res = _callocX(lful); 

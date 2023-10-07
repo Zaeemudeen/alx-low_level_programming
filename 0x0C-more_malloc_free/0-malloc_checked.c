d18cd62612be58379e275b8986e6b8241112d0a4
Zaeemudeen <142699408+Zaeemudeen@users.noreply.github.com>
2023-10-07 05:07:52 +0100

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,12 +3,11 @@
 #include "main.h"
 
 /**
- * malloc_checked -  allocates memory using malloc.
+ * malloc_checked -  allocates memory using malloc
  * @b: size
  * Return: Pointer to allocated memory or normal process termination
  * with a status value of 98
  */
- 
 void *malloc_checked(unsigned int b)
 {
 	void *str;

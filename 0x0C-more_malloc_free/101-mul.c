87db275b0c5a958bb4d7fb3cc19feb4646b13a09
Zaeemudeen <142699408+Zaeemudeen@users.noreply.github.com>
2023-10-07 14:03:54 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -66,31 +66,33 @@ int main(int argc, char **argv)
 		printf("Error\n"), exit(98);
 
 	if (atoi(argv[1]) == 0 || atoi(argv[2]) == 0)
-		printf("0\n"), exit(0); 
-         l1 = strlen(argv[1]), l2 = strlen(argv[2]); 
-         lful = l1 + l2; 
-         res = _callocX(lful); 
-         if (res == 0) 
-                 printf("Error\n"), exit(98); 
-         for (i = l2 - 1; i >= 0; i--) 
-         { 
-                 ten = 0, ten2 = 0; 
-                 for (j = l1 - 1; j >= 0; j--) 
-                 { 
-                         tl = i + j + 1; 
-                         mul = (argv[1][j] - '0') * (argv[2][i] - '0') + ten; 
-                         ten =  mul / 10; 
-                         add = (res[tl] - '0') + (mul % 10) + ten2; 
-                         ten2 = add / 10; 
-                         res[tl] = (add % 10) + '0'; 
-                 } 
-                 res[tl - 1] = (ten + ten2) + '0'; 
-         } 
-         if (res[0] == '0') 
-                 zer = 1; 
-         for (; zer < lful; zer++) 
-                 printf("%c", res[zer]); 
-         printf("\n"); 
-         free(res); 
-         return (0); 
- }
+		printf("0\n"), exit(0);
+
+	l1 = strlen(argv[1]), l2 = strlen(argv[2]);
+	lful = l1 + l2;
+	res = _callocX(lful);
+	if (res == 0)
+		printf("Error\n"), exit(98);
+	for (i = l2 - 1; i >= 0; i--)
+	{
+		ten = 0, ten2 = 0;
+		for (j = l1 - 1; j >= 0; j--)
+		{
+			tl = i + j + 1;
+			mul = (argv[1][j] - '0') * (argv[2][i] - '0') + ten;
+			ten =  mul / 10;
+			add = (res[tl] - '0') + (mul % 10) + ten2;
+			ten2 = add / 10;
+			res[tl] = (add % 10) + '0';
+		}
+		res[tl - 1] = (ten + ten2) + '0';
+	}
+
+	if (res[0] == '0')
+		zer = 1;
+	for (; zer < lful; zer++)
+		printf("%c", res[zer]);
+	printf("\n");
+	free(res);
+	return (0);
+}
